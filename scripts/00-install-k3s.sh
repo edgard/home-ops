a5935fecc20615be8056f4e6bffd69a326754f09
Edgard Castro <castro@edgard.org>
2022-06-06 19:44:17 +0200

    Update taskfile to include functionality from scripts

--- scripts/00-install-k3s.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL=stable INSTALL_K3S_SYMLINK=skip INSTALL_K3S_EXEC="--write-kubeconfig-mode 0644 --flannel-backend=none --disable-network-policy --disable-helm-controller --disable metrics-server --disable local-storage --disable traefik --disable servicelb" sh -s -
