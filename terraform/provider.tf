f9d48b0433c7c5300ebfba0e73f2214246218f70
Edgard Castro <castro@edgard.org>
2023-10-04 14:28:19 +0200

    re-add local dns/dhcp server

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -8,6 +8,14 @@ terraform {
       source  = "cloudflare/cloudflare"
       version = ">= 3.16.0"
     }
+    dns = {
+      source  = "hashicorp/dns"
+      version = ">= 3.2.3"
+    }
+    remote = {
+      source  = "tenstad/remote"
+      version = ">= 0.1.0"
+    }
     sops = {
       source  = "carlpett/sops"
       version = ">= 0.7.1"
@@ -23,4 +31,22 @@ data "sops_file" "terraform_secrets" {
 provider "cloudflare" {
   email   = data.sops_file.terraform_secrets.data["email"]
   api_key = data.sops_file.terraform_secrets.data["cloudflare_api_key"]
+}
+
+provider "dns" {
+  update {
+    server        = data.sops_file.terraform_secrets.data["dns_server_ip"]
+    key_name      = "rndc-key."
+    key_algorithm = "hmac-sha256"
+    key_secret    = data.sops_file.terraform_secrets.data["dns_key_secret"]
+  }
+}
+
+provider "remote" {
+  conn {
+    host        = data.sops_file.terraform_secrets.data["dns_server_ip"]
+    user        = "pi"
+    private_key = data.sops_file.terraform_secrets.data["ssh_private_key"]
+    sudo        = true
+  }
 }
