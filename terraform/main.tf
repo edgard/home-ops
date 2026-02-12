a962f638276acf1c0872d7176daf1952aae623e3
Edgard Castro <castro@edgard.org>
2026-02-12 05:09:53 +0100

    chore(deps): update dependencies (non-major)

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,7 +13,7 @@ terraform {
     }
     tailscale = {
       source  = "tailscale/tailscale"
-      version = "0.26.0"
+      version = "0.27.0"
     }
     bitwarden-secrets = {
       source  = "bitwarden/bitwarden-secrets"
